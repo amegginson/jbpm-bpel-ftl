9d1e0f3e4a63bb9244e9a51bcf6c56917e4df5e3
thomas.diesler@jboss.com <thomas.diesler@jboss.com@c2c9ca6b-772a-0410-851e-9041bc359291>
2008-06-30 14:42:29 +0000

    Add module distribution
    git-svn-id: http://anonsvn.jboss.org/repos/jbpm/jbpm3/trunk@1415 c2c9ca6b-772a-0410-851e-9041bc359291

--- modules/jpdl/db/generate.schema.update.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-ant -f build.upgrade.xml generate.old.schema
-ant -f build.upgrade.xml generate.update > target/schema.upgrade.log
-echo schema update generated in target/schema.upgrade.log
