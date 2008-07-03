5b912cb44425d48ca0c43d4260425939fa7beed2
alex.guizar@jboss.com <alex.guizar@jboss.com@c2c9ca6b-772a-0410-851e-9041bc359291>
2008-07-03 11:35:51 +0000

    made enterprise scheduler service delete executed timer from db: JBPM-1092
    git-svn-id: http://anonsvn.jboss.org/repos/jbpm/jbpm3/trunk@1492 c2c9ca6b-772a-0410-851e-9041bc359291

--- modules/enterprise/src/main/java/org/jbpm/scheduler/ejbtimer/ExecuteTimerCommand.java
+++ modules/enterprise/src/main/java/org/jbpm/scheduler/ejbtimer/ExecuteTimerCommand.java
@@ -4,25 +4,30 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jbpm.JbpmContext;
 import org.jbpm.command.Command;
-import org.jbpm.job.Timer;
+import org.jbpm.db.JobSession;
+import org.jbpm.job.Job;
 
 public class ExecuteTimerCommand implements Command {
 
   private static final long serialVersionUID = 1L;
   
-  long timerId;
+  final long timerId;
 
   public ExecuteTimerCommand(long timerId) {
     this.timerId = timerId;
   }
 
   public Object execute(JbpmContext jbpmContext) throws Exception {
-    Timer timer = (Timer) jbpmContext.getJobSession().getJob(timerId);
+    JobSession jobSession = jbpmContext.getJobSession();
+    Job timer = jobSession.getJob(timerId);
     if (timer!=null) {
       log.debug("executing timer "+timerId);
-      timer.execute(jbpmContext);
+      if (timer.execute(jbpmContext)) {
+        log.debug("deleting timer "+timerId);
+        jobSession.deleteJob(timer);
+      }
     } else {
-      log.info("execution of timer "+timerId+" was skipped cause the timer was deleted from the database");
+      log.info("timer "+timerId+" was deleted, cannot execute it");
     }
     return timer;
   }
